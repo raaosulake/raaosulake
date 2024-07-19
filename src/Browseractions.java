c3e60781c341af546154bb17d5e6392fd75ff6c6
raaosulake <raaosulake@gmail.com>
2024-07-19 11:47:46 +0530

    this is commit from raaosulake

--- src/Browseractions.java
+++ src/Browseractions.java
@@ -9,8 +9,11 @@ WebDriver driver = new ChromeDriver();
 driver.manage().window().maximize();
 driver.get("https://rahulshettyacademy.com/practice-project");
 driver.navigate().to("https://www.google.com");
+System.out.println(driver.getTitle());
 driver.navigate().back();
+System.out.println(driver.getTitle());
 driver.navigate().forward();
+System.out.println(driver.getTitle());
 driver.manage().window().minimize();
 driver.close();
 	}
